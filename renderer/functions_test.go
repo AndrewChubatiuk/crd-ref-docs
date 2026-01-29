9bad0d3057b8b4aab541d5dd985d1d71ce63e551
Andrii Chubatiuk <andrew.chubatiuk@gmail.com>
2026-01-29 19:38:26 +0200

    update go.mod

--- renderer/functions_test.go
+++ renderer/functions_test.go
@@ -19,8 +19,8 @@ package renderer
 import (
 	"testing"
 
-	"github.com/elastic/crd-ref-docs/config"
-	"github.com/elastic/crd-ref-docs/types"
+	"github.com/AndrewChubatiuk/crd-ref-docs/config"
+	"github.com/AndrewChubatiuk/crd-ref-docs/types"
 	"github.com/stretchr/testify/require"
 )
 
