963c59b5f55328245a6f03b88e44c1d86b3f394b
Andrii Chubatiuk <andrew.chubatiuk@gmail.com>
2026-02-04 10:16:55 +0200

    support custom markers with values

--- config/config.go
+++ config/config.go
@@ -54,8 +54,9 @@ type ProcessorConfig struct {
 }
 
 type Marker struct {
-	Name   string
-	Target TargetType
+	Name     string
+	Target   TargetType
+	HasValue bool `json:"hasValue"`
 }
 
 type TargetType string
